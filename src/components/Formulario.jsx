982e49de7d2af79ae0a1bac2934fbdf976d14b80
TeewsPepper <gomez.pepper@gmail.com>
2024-09-02 04:35:25 -0300

    Validación corregida

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,8 +1,7 @@
-
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import DOMPurify from "dompurify";
-import warningIcon from "../../public/images/icon-error.svg"; 
+import warningIcon from "../assets/images/icon-error.svg"; 
 import "../styles/Formulario.css";
 
 function Formulario() {
@@ -11,15 +10,8 @@ function Formulario() {
     handleSubmit,
     formState: { errors },
     reset,
-    clearErrors,
   } = useForm();
   const [successMessage, setSuccessMessage] = useState("");
-
-  // Crear referencias para cada input
-  const firstNameRef = useRef(null);
-  const lastNameRef = useRef(null);
-  const emailRef = useRef(null);
-  const passwordRef = useRef(null);
 
   const onSubmit = (data) => {
     const sanitizedData = {
@@ -39,176 +31,150 @@ function Formulario() {
     }, 5000);
   };
 
-  // Manejar el desenfoque de los inputs si hay errores
-  useEffect(() => {
-    if (errors.firstName) {
-      firstNameRef.current.blur();
-    }
-    if (errors.lastName) {
-      lastNameRef.current.blur();
-    }
-    if (errors.email) {
-      emailRef.current.blur();
-    }
-    if (errors.password) {
-      passwordRef.current.blur();
-    }
-  }, [errors]);
-
-  const clearError = (field) => () => clearErrors(field);
-
   return (
     <div className="container">
-      <div className="header">
-        <h1>Learn to code by watching others</h1>
-        <p className="paragraph">
-          See how experienced developers solve problems in real-time. Watching
-          scripted tutorials is great, but understanding how developers think is
-          invaluable.
-        </p>
+      <h1>Learn to code by watching others</h1>
+      <p className="paragraph">
+        See how experienced developers solve problems in real-time. Watching
+        scripted tutorials is great, but understanding how developers think is
+        invaluable.
+      </p>
+      <div className="price-info">
+        <strong>Try it free 7 days</strong> then $20/mo. thereafter
       </div>
 
-      <div className="formAndPrice">
-        <div className="price-info">
-          <strong>Try it free 7 days</strong> then $20/mo. thereafter
-        </div>
-        <form className="form" onSubmit={handleSubmit(onSubmit)}>
-          <div className="form-group">
-            <div className="input-wrapper">
-              <input
-                type="text"
-                placeholder="First Name"
-                {...register("firstName", {
-                  required: "First Name is required",
-                  maxLength: {
-                    value: 30,
-                    message: "First Name cannot exceed 30 characters",
-                  },
-                  pattern: {
-                    value: /^[A-Za-z\s]+$/,
-                    message: "First Name should only contain letters",
-                  },
-                })}
-                className={errors.firstName ? "input-error" : ""}
-                ref={firstNameRef}
-                onFocus={clearError("firstName")} // Limpiar error al hacer foco
-              />
-              {errors.firstName && (
-                <img src={warningIcon} alt="Warning" className="warning-icon" />
-              )}
-            </div>
+      
+
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-group">
+          <div className="input-wrapper">
+            <input
+              type="text"
+              placeholder="First Name"
+              {...register("firstName", {
+                required: "First Name is required",
+                maxLength: {
+                  value: 30,
+                  message: "First Name cannot exceed 30 characters",
+                },
+                pattern: {
+                  value: /^[A-Za-z\s]+$/,
+                  message: "First Name should only contain letters",
+                },
+              })}
+              className={errors.firstName ? "input-error" : ""}
+            />
             {errors.firstName && (
-              <div className="error">
-                <span className="error-message">
-                  {errors.firstName.message}
-                </span>
-              </div>
-            )}
-          </div>
-          <div className="form-group">
-            <div className="input-wrapper">
-              <input
-                type="text"
-                placeholder="Last Name"
-                {...register("lastName", {
-                  required: "Last Name is required",
-                  maxLength: {
-                    value: 30,
-                    message: "Last Name cannot exceed 30 characters",
-                  },
-                  pattern: {
-                    value: /^[A-Za-z\s]+$/,
-                    message: "Last Name should only contain letters",
-                  },
-                })}
-                className={errors.lastName ? "input-error" : ""}
-                ref={lastNameRef}
-                onFocus={clearError("lastName")} // Limpiar error al hacer foco
+              <img
+                src={warningIcon}
+                alt="Warning"
+                className="warning-icon"
               />
-              {errors.lastName && (
-                <img src={warningIcon} alt="Warning" className="warning-icon" />
-              )}
-            </div>
-            {errors.lastName && (
-              <div className="error">
-                <span className="error-message">{errors.lastName.message}</span>
-              </div>
             )}
           </div>
-          <div className="form-group">
-            <div className="input-wrapper">
-              <input
-                type="email"
-                placeholder="Email Address"
-                {...register("email", {
-                  required: "Email is required",
-                  pattern: {
-                    value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
-                    message: "Invalid email address",
-                  },
-                })}
-                className={errors.email ? "input-error" : ""}
-                ref={emailRef}
-                onFocus={clearError("email")} // Limpiar error al hacer foco
-              />
-              {errors.email && (
-                <img src={warningIcon} alt="Warning" className="warning-icon" />
-              )}
-            </div>
-            {errors.email && (
-              <div className="error">
-                <span className="error-message">{errors.email.message}</span>
-              </div>
-            )}
-          </div>
-          <div className="form-group">
-            <div className="input-wrapper">
-              <input
-                type="password"
-                placeholder="Password"
-                {...register("password", {
-                  required: "Password is required",
-                  minLength: {
-                    value: 8,
-                    message: "Password must be at least 8 characters",
-                  },
-                  pattern: {
-                    value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/,
-                    message:
-                      "Password must contain at least one letter and one number",
-                  },
-                })}
-                className={errors.password ? "input-error" : ""}
-                ref={passwordRef}
-                onFocus={clearError("password")} // Limpiar error al hacer foco
-              />
-              {errors.password && (
-                <img src={warningIcon} alt="Warning" className="warning-icon" />
-              )}
-            </div>
-            {errors.password && (
-              <div className="error">
-                <span className="error-message">{errors.password.message}</span>
-              </div>
-            )}
-          </div>
-          {successMessage && (
-            <p className="success-message">{successMessage}</p>
+          {errors.firstName && (
+            <span className="error-message">{errors.firstName.message}</span>
           )}
-          <button type="submit" className="btn">
-            Claim your free trial
-          </button>
-          <p className="terms">
-            By clicking the button, you are agreeing to our{" "}
-            <a href="#">Terms and Services</a>
-          </p>
-        </form>
-      </div>
+        </div>
+        <div className="form-group">
+          <div className="input-wrapper">
+            <input
+              type="text"
+              placeholder="Last Name"
+              {...register("lastName", {
+                required: "Last Name is required",
+                maxLength: {
+                  value: 30,
+                  message: "Last Name cannot exceed 30 characters",
+                },
+                pattern: {
+                  value: /^[A-Za-z\s]+$/,
+                  message: "Last Name should only contain letters",
+                },
+              })}
+              className={errors.lastName ? "input-error" : ""}
+            />
+            {errors.lastName && (
+              <img
+                src={warningIcon}
+                alt="Warning"
+                className="warning-icon"
+              />
+            )}
+          </div>
+          {errors.lastName && (
+            <span className="error-message">{errors.lastName.message}</span>
+          )}
+        </div>
+        <div className="form-group">
+          <div className="input-wrapper">
+            <input
+              type="email"
+              placeholder="Email Address"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value:
+                    /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
+                  message: "Invalid email address",
+                },
+              })}
+              className={errors.email ? "input-error" : ""}
+            />
+            {errors.email && (
+              <img
+                src={warningIcon}
+                alt="Warning"
+                className="warning-icon"
+              />
+            )}
+          </div>
+          {errors.email && (
+            <span className="error-message">{errors.email.message}</span>
+          )}
+        </div>
+        <div className="form-group">
+          <div className="input-wrapper">
+            <input
+              type="password"
+              placeholder="Password"
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 8,
+                  message: "Password must be at least 8 characters",
+                },
+                pattern: {
+                  value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/,
+                  message:
+                    "Password must contain at least one letter and one number",
+                },
+              })}
+              className={errors.password ? "input-error" : ""}
+            />
+            {errors.password && (
+              <img
+                src={warningIcon}
+                alt="Warning"
+                className="warning-icon"
+              />
+            )}
+          </div>
+          {errors.password && (
+            <span className="error-message">{errors.password.message}</span>
+          )}
+        {successMessage && <span className="success-message">{successMessage}</span>}
+        </div>
+        <button type="submit" className="btn">
+          Claim your free trial
+        </button>
+        <p className="terms">
+          By clicking the button, you are agreeing to our{" "}
+          <a href="#">Terms and Services</a>
+        </p>
+      </form>
     </div>
   );
 }
 
 export default Formulario;
-
-
-
